f05e22681810a3e237bbdf5522cbfba215e9259a
Dmitry Shirokov <Aka-mant@mail.ru>
2026-06-28 19:30:27 +0300

    - Добавил permissions

--- sections/urls.py
+++ sections/urls.py
@@ -36,21 +36,21 @@ int_pk = '<int:pk>/'
 
 
 urlpatterns = [
-    # sections urlspatterns
+    # sections urlpatterns
     path(p.join(sections), SectionListAPIView.as_view(), name='section_list'),
     path(p.join(sections, create), SectionCreateAPIView.as_view(), name='section_create'),
     path(p.join(sections, int_pk), SectionRetrieveAPIView.as_view(), name='section_detail'),
     path(p.join(sections, int_pk, update), SectionUpdateAPIView.as_view(), name='section_update'),
     path(p.join(sections, int_pk, delete), SectionDeleteAPIView.as_view(), name='section_delete'),
-    # content urlspatterns
+    # content urlpatterns
     path(p.join(content), ContentListAPIView.as_view(), name='content_list'),
     path(p.join(content, create), ContentCreateAPIView.as_view(), name='content_create'),
     path(p.join(content, int_pk), ContentRetrieveAPIView.as_view(), name='content_detail'),
     path(p.join(content, int_pk, update), ContentUpdateAPIView.as_view(), name='content_update'),
     path(p.join(content, int_pk, delete), ContentDeleteAPIView.as_view(), name='content_delete'),
-    # question urlspatterns
+    # question urlpatterns
     path(p.join(questions), QuestionListAPIView.as_view(), name='question_list'),
     path(p.join(questions, int_pk), QuestionRetrieveAPIView.as_view(), name='question_detail'),
-    
+
 
 ] + router.urls
